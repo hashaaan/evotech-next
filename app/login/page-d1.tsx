cc5954a3328e64591e3ab373c542b45ccf2c6ac1
hashaaan <shalithadev@gmail.com>
2024-12-04 23:16:28 +0530

    Add login page UI

---
+++ app/login/page-d1.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-yellow-200 text-black text-center">
+      <div className="bg-green-300 px-5 py-3 rounded-lg">Login</div>
+    </div>
+  );
+}
