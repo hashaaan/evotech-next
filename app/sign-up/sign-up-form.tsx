46f5e61dfc5f68e76605a86db93e1db640c87c63
hashaaan <shalithadev@gmail.com>
2024-12-21 17:54:46 +0530

    Add form submission for signup

--- app/sign-up/sign-up-form.tsx
+++ app/sign-up/sign-up-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,9 +11,24 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React from "react";
+import React, { FormEvent } from "react";
 
 export default function SignUpForm() {
+  const handleSubmitForm = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent form submission default behavior
+    const formData = new FormData(event.currentTarget);
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirm-password");
+
+    if (password === confirmPassword) {
+      // Continue
+      console.log({ error: false, name, email, password, confirmPassword });
+    } else {
+      console.log({ error: true, message: "Passwords doesn't match." });
+    }
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Card className="bg-blue-50/50 w-[350px]">
@@ -21,23 +38,24 @@ export default function SignUpForm() {
             Enter your information to get started
           </CardDescription>
         </CardHeader>
-        <form>
+        <form onSubmit={handleSubmitForm}>
           <CardContent>
             <div className="grid w-full items-center gap-4">
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="name">Name</Label>
-                <Input id="name" placeholder="John Doe" />
+                <Input id="name" name="name" placeholder="John Doe" />
               </div>
 
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" placeholder="john@example.com" />
+                <Input id="email" name="email" placeholder="john@example.com" />
               </div>
 
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="password">Password</Label>
                 <Input
                   id="password"
+                  name="password"
                   type="password"
                   placeholder="Enter new password"
                 />
@@ -47,6 +65,7 @@ export default function SignUpForm() {
                 <Label htmlFor="confirm-password">Confirm password</Label>
                 <Input
                   id="confirm-password"
+                  name="confirm-password"
                   type="password"
                   placeholder="Enter password again to confirm"
                 />
