32dd97d1cb9124b9e20833c1908020002779a768
hashaaan <shalithadev@gmail.com>
2025-01-10 01:15:06 +0530

    Better auth configuration

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,23 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
 import RegisterForm from "./register-form";
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
   // Create sign up form using shadcn
   // Add shadcn Card, Label, Input
   // Use zod to validate form
   // Use mongodb to store user
   // Install bcrypt
+
+  const session = await auth.api.getSession({
+    headers: await headers(), // you need to pass the headers object.
+  });
+
+  if (session) {
+    // Redirect to dashboard if user has already logged in
+    redirect("/dashboard");
+  }
 
   return (
     <div className="container mx-auto">
