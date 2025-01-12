5019e82688b286445b1f6c60bf0e73b212e2d632
hashaaan <shalithadev@gmail.com>
2025-01-12 17:49:39 +0530

    Refactor mongodb client

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,15 +1,13 @@
 import { MongoClient } from "mongodb";
 
-async function clientPromise() {
+export async function clientPromise() {
   const MONGODB_URI = process.env.MONGODB_URI;
   const options = {};
 
   if (!MONGODB_URI) {
-    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+    throw new Error("Invalid/Missing environment variable: MONGODB_URI");
   }
 
   const client = new MongoClient(MONGODB_URI, options);
   return client.connect();
 }
-
-export default clientPromise;
